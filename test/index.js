b47205507d80e39f73f0c79f8437d45de86ee926
Michiel Kalkman <michielkalkman@gmail.nl>
2014-07-23 11:30:04 +0200

    Add seconds, use UTC
      - Add test cases
      - Use UTC values where available
      - Cleanup typos
      - Add seconds as grouping interval

--- test/index.js
+++ test/index.js
@@ -10,11 +10,26 @@ var aggregates = Util.aggregateFunctions;
 
 var RANGESIZE = 1000;
 
-var mom     = moment(new Date(0));
+var mom     = null; // moment(new Date(0));
 var counter = 0;
+
+var incSecond = function() {
+  mom.add('seconds', 1);
+  return +mom.toDate();
+};
 
 var incDay = function() {
   mom.add('days', 1);
+  return +mom.toDate();
+};
+
+var incMonth = function() {
+  mom.add('months', 1);
+  return +mom.toDate();
+};
+
+var incYear = function() {
+  mom.add('years', 1);
   return +mom.toDate();
 };
 
@@ -25,10 +40,11 @@ function generateDatePoint(fn) {
   };
 }
 
-function generateDatePoints(fn) {
+function generateDatePoints(fn, amount) {
   var datePoints = [];
+  amount = amount || RANGESIZE;
 
-  for (var i=0; i<RANGESIZE; i++) {
+  for (var i=0; i<amount; i++) {
     datePoints.push(generateDatePoint(fn));
   }
 
@@ -46,6 +62,22 @@ describe('Grouper', function() {
         done();
       });
 
+    describe('groupBy#second/second', function() {
+
+        it('should have 1000 elements when seconds grouped by second', function() {
+
+            datePoints = generateDatePoints(incSecond);
+            var grouper = new Grouper({
+                groupBy     : 'second'
+              });
+
+            grouper.setData(datePoints);
+            grouper.processData();
+
+            assert.equal(grouper.getData().length, RANGESIZE);
+          });
+      });
+
     describe('groupBy#minute', function() {
 
         it('should have 1000 elements when days grouped by minute', function() {
@@ -57,7 +89,6 @@ describe('Grouper', function() {
 
             grouper.setData(datePoints);
             grouper.processData();
-
 
             assert.equal(grouper.getData().length, RANGESIZE);
           });
@@ -93,6 +124,58 @@ describe('Grouper', function() {
             grouper.processData();
 
             assert.equal(grouper.getData().length, 40);
+          });
+      });
+
+    describe('groupBy#months/month', function() {
+
+        it('should have 12 elements when months grouped by month', function() {
+
+            datePoints = generateDatePoints(incMonth, 12);
+
+            var grouper = new Grouper({
+                groupBy     : 'month'
+              });
+
+            grouper.setData(datePoints);
+            grouper.processData();
+
+            assert.equal(grouper.getData().length, 12);
+          });
+      });
+
+    describe('groupBy#months/day', function() {
+
+        it('should have 12 element when months grouped by day', function() {
+
+            datePoints = generateDatePoints(incMonth, 12);
+
+            var grouper = new Grouper({
+                groupBy     : 'day'
+              });
+
+            grouper.setData(datePoints);
+            grouper.processData();
+
+            assert.equal(grouper.getData().length, 12);
+          });
+      });
+
+
+    describe('groupBy#years/year', function() {
+
+        it('should have 12 element when years grouped by year', function() {
+
+            datePoints = generateDatePoints(incYear, 12);
+
+            var grouper = new Grouper({
+                groupBy     : 'year'
+              });
+
+            grouper.setData(datePoints);
+            grouper.processData();
+
+            assert.equal(grouper.getData().length, 12);
           });
       });
 
